d78a230bb2550add670a2d46874b5e01144504c3
p4kz <wolpak@hotmail.com>
2022-07-07 17:30:19 -0300

    create interface

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-import './src/styles/global.css'
-import { setupCounter } from './src/modules/counter.js'
+import App from './src/App'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+document.body.append(App())
