ef57d13ad0aabebdc7e430f8eadb9b63ec98a2bd
p4kz <wolpak@hotmail.com>
2022-07-29 16:07:44 -0300

    fix styles - test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
 
 export const Header = () => {
   const Header = function () {
-    console.log( "created" )
     return Reflect.construct( HTMLElement, [], Header )
   }
 
   Header.prototype = Object.create( HTMLElement.prototype )
 
   Header.prototype.connectedCallback = function () {
-    console.log( "connected" )
-    this.innerHTML = "hello"
+    this.innerHTML = `
+      <div class="navbar">
+        <div class="navbar__logo">
+          <a class="navbar__link" target="_blank" href="https://github.com/p4kz">p4k</a>
+        </div>
+        <ul class="navbar__list">
+          <li>
+            <a class="navbar__link" href="#">Home</a>
+            <a class="navbar__link" href="#">Sobre</a>
+          </li>
+        </ul>
+      </div>
+    `
   }
 
-  return customElements.define( "nav-bar", Header )
+  return customElements.define( 'nav-bar', Header )
 }
-
